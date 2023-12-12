9eabc6fa45086d28f88f8c111d9bdbc97bf71c3d
Yinpu Li <yli3@mfm9h02vkc.chewy.local>
2023-12-12 11:13:53 -0800

    With new random samples.

--- utils/constant.py
+++ utils/constant.py
@@ -1,6 +1,6 @@
 import os
 
-PROJECT_BASE_DIR = os.path.expanduser('/Users/yli3/Library/CloudStorage/GoogleDrive-yinpu.chewy@gmail.com/My Drive/Documents/Projects/PulseGuard-Sales')
+PROJECT_BASE_DIR = os.path.expanduser('/Users/yli3/Library/CloudStorage/GoogleDrive-yinpu.chewy@gmail.com/My Drive/Documents/Projects/PulseGuard-Sales-1')
 
 
 
